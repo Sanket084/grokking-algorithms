e04beaa1c6767b4e23cebf6680ae8d8e4e742a2d
Sanket084 <kadlag.sanket82@gmail.com>
2024-07-04 23:29:13 +0530

    added 05-hash-tables/

---
+++ 05-hash-tables/phonebook.py
@@ -0,0 +1,11 @@
+# dictionary is a built-in hash table in Python
+# this program is for self explanatory purpose and not from the book
+
+phonebook = dict()
+
+phonebook["Sanket Kadlag"] = 8888881114
+phonebook["Adam Sandler"] = 8087332777
+phonebook["Emergency"] = 911
+
+print (phonebook["Sanket Kadlag"])
+print (phonebook)
